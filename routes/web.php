6c46800dda813f142f7344ff2f06214df3550e7d
mrjuniors <sergio.rebelo@mysp.pt>
2023-03-13 16:02:22 +0000

    first commit

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('create-post', function () {
+    $user = User::first();
+
+    $user->posts() -> create([
+        'title'=>Str::random(150),
+        'body' =>Str::random(400),
+    ]);
+
+    return 'ok';
+});
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
